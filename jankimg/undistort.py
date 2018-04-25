2f266ca256e507d04d62441fa630e1b089bab19e
dylanhart <dylan96hart@gmail.com>
2018-04-24 20:25:30 -0700

    even better undistort

--- jankimg/undistort.py
+++ jankimg/undistort.py
@@ -11,10 +11,12 @@ DIM=(720, 480)
 K=np.array([[347.0464967569512, 0.0, 345.6381028423219], [0.0, 349.23483039090365, 193.6250314050006], [0.0, 0.0, 1.0]])
 D=np.array([[-0.03483461030252116], [0.0029723614826183634], [-0.004590284238468527], [0.0008656745800786099]])
 
+map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
+
+
 def undistort(img):
-    h,w = img.shape[:2]
-    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
     return cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+
 
 if __name__ == '__main__':
     for p in sys.argv[1:]:
